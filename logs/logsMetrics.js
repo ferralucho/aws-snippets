c5f66322be6eb69d1a68fe21d4d75f6643aff0e6
Luciano Ferrari <lucianodarioferrari@gmail.com>
2021-11-06 16:46:19 -0300

    Create logsMetrics.js

---
+++ logs/logsMetrics.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+const { metricScope } = require("aws-embedded-metrics");
+
+const TABLE_NAME = process.env.TABLE_NAME;
+
+exports.saveHello = async (event) => {
+  const name = event.queryStringParameters.name;
+
+  const item = {
+    id: name,
+    name: name,
+    date: Date.now(),
+  };
+
+  console.log(item);
+  
+  const savedItem = await saveItem(item);
+
+  return getResponse(savedItem);
+};
+
+exports.getHello = metricScope(metrics =>
+    async (event) => {
+        metrics.setNamespace('gretter')
+
+        const name = event.queryStringParameters.name;
+
+        try {
+            const item = await getItem(name);
+        
+            if (item.date) {
+                const d = new Date(item.date);
+
+                const message = `Was greeted on ${d.getDate()}/${
+                    d.getMonth() + 1
+                }/${d.getFullYear()}`;
+
+                metrics.putDimensions({Service: "GetName"});
+                metrics.putMetric("GetName", 1, "put");
+                metrics.setProperty("Name", name);
+                metrics.setProperty("Result", item);
+
+                return getResponse(message);
+            }
+        } catch (e) {
+            console.log(e);
+
+            const message = "Nobody was greeted with that name";
+            
+            metrics.putDimensions({Service: "GetName"});
+            metrics.putMetric("NotFound", 1, "notFound");
+            metrics.setProperty("Name", name);
+
+            return getResponse(message);
+        }
+    }
+);
+
+async function getItem(name) {
+  const params = {
+    Key: {
+      id: name,
+    },
+    TableName: TABLE_NAME,
+  };
+
+  return dynamo
+    .get(params)
+    .promise()
+    .then((result) => {
+      return result.Item;
+    });
+}
+
+async function saveItem(item) {
+  const params = {
+    TableName: TABLE_NAME,
+    Item: item,
+  };
+
+  console.log(params);
+
+  return dynamo
+    .put(params)
+    .promise()
+    .then(() => {
+      return item;
+    });
+}
+
+function getResponse(message) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(message),
+      };
+}
