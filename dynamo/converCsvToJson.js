ef48775be3d4ca8a9f618b7c5b7693c18ee6c5c3
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-02-26 16:09:11 -0300

    for file items

--- dynamo/converCsvToJson.js
+++ dynamo/converCsvToJson.js
@@ -1,37 +1,30 @@
 const csv = require("csvtojson");
 
 const csvFilePath =
-  "./string-lookup-alert-format.csv";
+  "./string-format.csv";
 
 var items = [];
 
 async function parseCsvToJson() {
-  await csv({ noheader: false })
+  let row;
+  await csv()
     .fromFile(csvFilePath)
-    .then((row) => {
-      var item = {
-        PutRequest: {
-          Item: {
-            classification: row[0],
-            idSort: row[1],
-            alertDefn: row[2],
-            createdAt: row[3],
-            eventId: row[4],
-            id: row[5],
-            kbArticles: row[6],
-            locale: row[7],
-            localeSort: row[8],
-            msgPrivate: row[9],
-            msgPublic: row[10],
-            severity: row[11],
-            title: row[12],
-          },
-        },
-      };
+    .then((fileItems) => {
+      for (let i = 0; i < fileItems.length; i++) {
+        row = fileItems[i];
 
-      items.push(item);
+        let item = {
+          PutRequest: {
+            Item: {
+              ...row,
+            },
+          },
+        };
+
+        items.push(item);
+      }
     });
-    console.log(JSON.stringify(items));
+  console.log(JSON.stringify(items));
 }
 
 parseCsvToJson();
